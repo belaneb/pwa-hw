34d380ac11e2ea188d8130f88f0d3376f1d9d24f
belaneb <belane.belayhun@gmail.com>
2023-06-06 10:47:40 -0500

    content updated and completed

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# pwa-hw
+# Title: 
+pwa-hw
+
+## Description: 
+Build a single-page text editor application that runs in the browser both online and offline featuring a number of data persistence techniques that serve as redundancy in case one of the options is not supported by the browser.
---
+++ client/src/js/install.js
@@ -0,0 +1,25 @@
+const butInstall = document.getElementById('buttonInstall');
+
+// Logic for installing the PWA
+// DONE: Add an event handler to the `beforeinstallprompt` event
+window.addEventListener('beforeinstallprompt', (event) => {
+     // Store the triggered events
+     window.deferredPrompt = event;
+
+     // Remove the hidden class from the button.
+     butInstall.classList.toggle('hidden', false);
+});
+
+// DONE: Implement a click event handler on the `butInstall` element
+butInstall.addEventListener('click', async () => {
+        const promptEvent = window.deferredPrompt;
+        if (!promptEvent) {
+         return;
+        }
+});
+
+// DONE: Add an handler for the `appinstalled` event
+window.addEventListener('appinstalled', (event) => {
+    // Clear prompt
+  window.deferredPrompt = null;
+});
